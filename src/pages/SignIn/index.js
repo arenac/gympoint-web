226aa0f2d38fe0e2acd33cbaa7e8fca5317d80ec
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-26 23:54:02 +0100

    dispatch signin request

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
 import { Container } from './styles';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insert a valid e-mail')
+    .required('A valid e-mail is required'),
+  password: Yup.string()
+    .min(6, 'The passwor should have at least 6 characters')
+    .required('Password is required'),
+});
+
 export default function SigIn() {
-  function handleSubmit() {}
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <Container>
       <img src={logo} alt="GoBarber" />
       <p>GYMPOINT</p>
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <label htmlFor="email">YOUR EMAIL</label>
         <Input name="email" type="email" placeholder="example@email.com" />
         <label htmlFor="password">YOUR PASSWORD</label>
