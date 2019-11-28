889f06d59e169b2287423cf6f7f54603ea6a7f50
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-28 23:25:09 +0100

    Student layout

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Content,
+  StudentTable,
+  EditButton,
+  DeleteButton,
+} from './styles';
 
 export default function Students() {
-  return <h1>Students</h1>;
+  return (
+    <Container>
+      <Header>
+        <span>Stundent List</span>
+        <div>
+          <button type="button">Register</button>
+          <input type="text" />
+        </div>
+      </Header>
+      <Content>
+        <StudentTable>
+          <thead>
+            <th>NAME</th>
+            <th>AGE</th>
+            <th>E-MAIL</th>
+            <th />
+            <th />
+          </thead>
+          <body>
+            <tr>
+              <td>Nilo</td>
+              <td>35</td>
+              <td>neregato.nilo@gmail.com</td>
+              <td>
+                <EditButton type="button">edit</EditButton>
+              </td>
+              <td>
+                <DeleteButton type="button">delete</DeleteButton>
+              </td>
+            </tr>
+          </body>
+        </StudentTable>
+      </Content>
+    </Container>
+  );
 }
