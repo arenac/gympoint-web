67c79cef757310ce4760fe8cf2c177bece0e24bc
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-03 11:21:24 +0100

    Plans and PlanForm components with style

--- src/pages/Plans/components/PlanForm/index.js
+++ src/pages/Plans/components/PlanForm/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import {
+  registerRequest,
+  updateRequest,
+  showStudents,
+} from '~/store/modules/plan/actions';
 
-export default function PlanForm() {
-  return <h1>PlanFrom</h1>;
+import { Container, Header, Content, Button } from './styles';
+
+export default function PlanForm({ plan }) {
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    if (plan) {
+      dispatch(updateRequest(plan.id, data));
+    } else {
+      dispatch(registerRequest(data));
+    }
+  }
+
+  function handleReturn() {
+    dispatch(showStudents(true));
+  }
+
+  return (
+    <Container>
+      <Form id="student-form" initialData={plan} onSubmit={handleSubmit}>
+        <Header>
+          <strong>{plan ? 'Edit student' : 'Register student'}</strong>
+          <aside>
+            <Button type="button" isgray onClick={handleReturn}>
+              RETURN
+            </Button>
+            <Button type="submit">SAVE</Button>
+          </aside>
+        </Header>
+        <Content>
+          <label htmlFor="name">FULL NAME</label>
+          <Input name="name" />
+          <label htmlFor="email">E-MAIL</label>
+          <Input name="email" />
+
+          <div>
+            <div>
+              <label htmlFor="age">AGE</label>
+              <Input name="age" />
+            </div>
+            <div>
+              <label htmlFor="weight">WEIGHT</label>
+              <Input name="weight" />
+            </div>
+            <div>
+              <label htmlFor="height">HEIGHT</label>
+              <Input name="height" />
+            </div>
+          </div>
+        </Content>
+      </Form>
+    </Container>
+  );
 }
+
+PlanForm.propType = {
+  plan: PropTypes.objectOf(PropTypes.object),
+};
--- src/pages/Plans/index.js
+++ src/pages/Plans/index.js
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PlanForm from './components/PlanForm';
 
-// import { Container } from './styles';
+import {
+  getRequest,
+  showStudents,
+  deleteRequest,
+} from '~/store/modules/plan/actions';
+
+import {
+  Container,
+  Header,
+  Content,
+  StudentTable,
+  EditButton,
+  DeleteButton,
+} from './styles';
 
 export default function Plans() {
-  return <h1>Plans</h1>;
+  const dispatch = useDispatch();
+  const planState = useSelector(state => state.plan);
+  const [planToEdit, setPlanToEdit] = useState(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    dispatch(getRequest(page));
+  }, [dispatch, page]);
+
+  function handleEditStudent(student) {
+    setPlanToEdit(student);
+    dispatch(showStudents(false));
+  }
+
+  function handleDeleteStudent(id) {
+    dispatch(deleteRequest(id));
+  }
+
+  return (
+    <Container>
+      {planState.show ? (
+        <>
+          <Header>
+            <strong>Plan List</strong>
+            <aside>
+              <button type="button" onClick={() => handleEditStudent(null)}>
+                REGISTER
+              </button>
+            </aside>
+          </Header>
+          <Content>
+            <StudentTable>
+              <thead>
+                <tr>
+                  <th>TITLE</th>
+                  <th>DURATION</th>
+                  <th>PRICE</th>
+                  <th />
+                  <th id="delete-column" />
+                </tr>
+              </thead>
+              <tbody>
+                {planState.plans.map(plan => (
+                  <tr key={plan.id}>
+                    <td>{plan.title}</td>
+                    <td>{plan.duration}</td>
+                    <td>{plan.price}</td>
+                    <td align="right">
+                      <EditButton
+                        type="button"
+                        onClick={() => handleEditStudent(plan)}
+                      >
+                        edit
+                      </EditButton>
+                    </td>
+                    <td align="left">
+                      <DeleteButton
+                        type="button"
+                        onClick={() => handleDeleteStudent(plan.id)}
+                      >
+                        delete
+                      </DeleteButton>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </StudentTable>
+          </Content>
+        </>
+      ) : (
+        <PlanForm student={planToEdit} />
+      )}
+    </Container>
+  );
 }
