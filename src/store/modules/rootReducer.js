919b347761e6d19b076ce93fd7f671049e414864
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-03 14:41:52 +0100

    eroll ment combine  reducer and sagas

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -3,5 +3,6 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducer';
 import student from './student/reducer';
 import plan from './plan/reducer';
+import enrollment from './enrollment/reducer';
 
-export default combineReducers({ auth, student, plan });
+export default combineReducers({ auth, student, plan, enrollment });
