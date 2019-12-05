4d4864f4122df0e439293dd0c170c751c883a753
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-05 23:41:06 +0100

    refactor routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,8 @@ import SignIn from '~/pages/SignIn';
 import Students from '~/pages/Students';
 import Plans from '~/pages/Plans';
 import Enrollments from '~/pages/Enrollments';
+import EnrollmentsRegister from '~/pages/Enrollments/Register';
+import EnrollmentsEdit from '~/pages/Enrollments/Edit';
 import Help from '~/pages/Help';
 
 export default function Routes() {
@@ -15,7 +17,14 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/students" component={Students} isPrivate />
       <Route path="/plans" component={Plans} isPrivate />
-      <Route path="/enrollments" component={Enrollments} isPrivate />
+
+      <Route path="/enrollments" exact component={Enrollments} isPrivate />
+      <Route
+        path="/enrollments/register"
+        component={EnrollmentsRegister}
+        isPrivate
+      />
+      <Route path="/enrollments/edit" component={EnrollmentsEdit} isPrivate />
       <Route path="/help" component={Help} isPrivate />
     </Switch>
   );
