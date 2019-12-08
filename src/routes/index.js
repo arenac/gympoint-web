9d0c890752ed4c42b88c6218b560f44997b9aef1
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-08 23:04:26 +0100

    aditiona students and plans routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,11 @@ import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import Students from '~/pages/Students';
+import RegisterStudent from '~/pages/Students/components/RegisterStudent';
+import EditStudent from '~/pages/Students/components/EditStudent';
 import Plans from '~/pages/Plans';
+import RegisterPlan from '~/pages/Plans/components/RegisterPlan';
+import EditPlan from '~/pages/Plans/components/EditPlan';
 import Enrollments from '~/pages/Enrollments';
 import RegisterEnrollment from '~/pages/Enrollments/RegisterEnrollment';
 import EditEnrollment from '~/pages/Enrollments/EditEnrollment';
@@ -15,8 +19,14 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/students" component={Students} isPrivate />
-      <Route path="/plans" component={Plans} isPrivate />
+
+      <Route path="/students" exact component={Students} isPrivate />
+      <Route path="/students/register" component={RegisterStudent} isPrivate />
+      <Route path="/students/edit" component={EditStudent} isPrivate />
+
+      <Route path="/plans" exact component={Plans} isPrivate />
+      <Route path="/plans/register" component={RegisterPlan} isPrivate />
+      <Route path="/plans/edit" component={EditPlan} isPrivate />
 
       <Route path="/enrollments" exact component={Enrollments} isPrivate />
       <Route
