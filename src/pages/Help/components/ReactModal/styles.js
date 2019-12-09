08d09b831c3e91f93928100c7531c8dc5274404b
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-10 00:22:19 +0100

    ReactModal and styles

---
+++ src/pages/Help/components/ReactModal/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 10px;
+
+  span {
+    color: #ee4d64;
+    align-self: self-start;
+    margin: 0 0 10px;
+    font-weight: bold;
+  }
+
+  div {
+    font-size: 14px;
+    color: #444;
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 16px;
+    color: #666;
+    text-align: justify;
+    margin-top: 14px;
+    margin-bottom: 14px;
+  }
+
+  textarea {
+    margin-top: 20px !important;
+    padding: 0 10px;
+    padding-top: 5px;
+    margin: 0 0 10px;
+    height: 150px;
+    width: 100%;
+
+    border-radius: 4px;
+    border: 0.8px solid #ddd;
+
+    background: #fff;
+    color: #454444;
+
+    &::placeholder {
+      color: #999;
+    }
+  }
+
+  button#closeButton {
+    background: none;
+    border: none;
+    width: 30px;
+    height: 30px;
+    align-self: flex-end;
+
+    img {
+      color: #ddd;
+      transition: backdround 0.2s;
+
+      &:hover {
+        color: ${darken(0.5, '#ddd')};
+      }
+    }
+  }
+
+  button#answerButton {
+    margin: 5px 0 0;
+    height: 44px;
+    background: #de3b3b;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: backdround 0.2s;
+
+    &:hover {
+      background: ${darken(0.05, '#de3b3b')};
+    }
+  }
+`;
