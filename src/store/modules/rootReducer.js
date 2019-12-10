8947a4e1cf2af343dde8e771dcf1e0c77446ca9f
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-10 15:51:46 +0100

    combine help reducer and sagas

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -4,5 +4,6 @@ import auth from './auth/reducer';
 import student from './student/reducer';
 import plan from './plan/reducer';
 import enrollment from './enrollment/reducer';
+import help from './help/reducer';
 
-export default combineReducers({ auth, student, plan, enrollment });
+export default combineReducers({ auth, student, plan, enrollment, help });
